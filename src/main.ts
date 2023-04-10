9f9ac1ac8b4543167480b4232be3391b04336772
Mateus Bastos <pc@Mateus-Mac-mini.local>
2023-04-10 09:42:13 -0300

    vercel json

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['http://localhost:4200/', 'https://anatomyagenda.com.br/'],
-    methods: ['GET', 'POST'],
-    credentials: false,
-  });
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
